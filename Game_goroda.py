4fb37171fa70ea1c1fd24a4e15605dba4a4d41ec
aatelitsina <39148045+aatelitsina@users.noreply.github.com>
2018-05-25 17:17:17 +0300

    Add files via upload
    Черновик

---
+++ Game_goroda.py
@@ -0,0 +1,10 @@
+from Cities import Cities
+def goroga(in_message):
+    cities = Cities
+    for char in in_message:
+        char
+    # print(in_message.split())
+
+    # if in_message in cities:
+
+goroga('Москва')
